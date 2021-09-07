68ec5dcb17d3f751097539713f690b09c1dd65dc
crissesoto <soto.crisse@hotmail.com>
2021-09-07 18:18:18 +0200

    added favicon

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,15 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const {loading, cocktails} = useGlobalContext();
+  if (loading) {
+    return <Loading />
+  }
+
+  if (cocktails.length < 1) {
+    return <h2 className="no-cocktail-title">No cocktail matched your search criteria.</h2>
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
